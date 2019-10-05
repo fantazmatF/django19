711d319abe6fe5888d5910820d126353e928367d
Krzysiek Pawlos <krzysiekpawlos@interia.pl>
2019-10-05 14:55:07 +0200

    nastpny

---
+++ microblog/posts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic import View
+
+
+class HelloWorldView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'posts/index.html')
+
+
+def hello_world(request):
+    context = {
+        'name': "Krzysiek",
+    }
+    return render(request, 'posts/index.html', context)
+
+
+# Create your views here.
